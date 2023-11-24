0f0796b651a1ee08737420de4e148df16ef15d06
Ashwanth2310 <ashwanth@rajakumar.net>
2023-11-24 15:08:00 +0530

    first commit

---
+++ Main Game/GameplayE.js
@@ -0,0 +1,131 @@
+let moveSpeed = 3, gravity = 0.5;
+let plane = document.querySelector('.plane');
+let planeImage = document.getElementById('planes');
+
+const preloadImage = new Image();
+preloadImage.src = 'plane.png';
+
+preloadImage.onload = function() {
+    planeImage.src = preloadImage.src;
+    planeImage.style.display = 'none';
+};
+
+// Getting plane element properties
+let planeProps = plane.getBoundingClientRect();
+
+let background = document.querySelector('.background').getBoundingClientRect();
+
+let scoreVal = document.querySelector('.score_val');
+let message = document.querySelector('.prompt');
+let scoreTitle = document.querySelector('.score_head');
+
+let gameState = 'Start';
+planeImage.style.display = 'none';
+message.classList.add('promptStyle');
+
+document.addEventListener('keydown', (e) => {
+    
+    if(e.key == 'Enter' && gameState != 'Play'){
+        document.querySelectorAll('.building').forEach((e) => {
+            e.remove();
+        });
+        planeImage.style.display = 'block';
+        plane.style.top = '40vh';
+        gameState = 'Play';
+        message.innerHTML = ' ';
+        scoreTitle.innerHTML = 'Score : ';
+        scoreVal.innerHTML = '0';
+        message.classList.remove('promptStyle');
+        play();
+    }
+});
+
+function play(){
+    function move(){
+        if(gameState != 'Play') return;
+
+        let building = document.querySelectorAll('.building');
+        building.forEach((element) => {
+            let buildingProps = element.getBoundingClientRect();
+            planeProps = plane.getBoundingClientRect();
+
+            if(buildingProps.right <= 0){
+                element.remove();
+            }else{
+                if(planeProps.left < buildingProps.left + buildingProps.width && planeProps.left + planeProps.width > buildingProps.left && planeProps.top < buildingProps.top + buildingProps.height && planeProps.top + planeProps.height > buildingProps.top){
+                    gameState = 'End';
+                    planeImage.style.display = 'none';
+                    window.location.href = 'Lose Screen.html';
+                    return;
+                }else{
+                    if(buildingProps.right < planeProps.left && buildingProps.right + moveSpeed >= planeProps.left && element.increaseScore == '1'){
+                        scoreVal.innerHTML =+ scoreVal.innerHTML + 1;
+                    }
+                    element.style.left = buildingProps.left - moveSpeed + 'px';
+                }
+            }
+        });
+        requestAnimationFrame(move);
+    }
+    requestAnimationFrame(move);
+
+    let planeDy = 0;
+    function applyGravity(){
+        if(gameState != 'Play') return;
+        planeDy = planeDy + gravity;
+        document.addEventListener('keydown', (e) => {
+            if(e.key == 'ArrowUp' || e.key == ' '|| e.key == 'w'){
+                planeImage.src = 'plane.png';
+                planeDy = -7.6;
+            }
+        });
+
+        document.addEventListener('keyup', (e) => {
+            if(e.key == 'ArrowUp' || e.key == ' ' || e.key == 'w'){
+                planeImage.src = 'plane.png';
+            }
+        });
+
+        if(planeProps.top <= 0 || planeProps.bottom >= background.bottom){
+            gameState = 'End';
+            message.style.left = '28vw';
+            window.location.reload();
+            message.classList.remove('promptStyle');
+            return;
+        }
+        plane.style.top = planeProps.top + planeDy + 'px';
+        planeProps = plane.getBoundingClientRect();
+        requestAnimationFrame(applyGravity);
+    }
+    requestAnimationFrame(applyGravity);
+
+    let buildingSeparation = 0;
+
+    let buildingGap = 40;
+
+    function createBuilding(){
+        if(gameState != 'Play') return;
+
+        if(buildingSeparation > 115){
+            buildingSeparation = -10;
+
+            let buildingPos = Math.floor(Math.random() * 43) + 8;
+            let buildingSpriteInv = document.createElement('div');
+            buildingSpriteInv.className = 'building';
+            buildingSpriteInv.style.top = buildingPos - 70 + 'vh';
+            buildingSpriteInv.style.left = '100vw';
+
+            document.body.appendChild(buildingSpriteInv);
+            let buildingSprite = document.createElement('div');
+            buildingSprite.className = 'building';
+            buildingSprite.style.top = buildingPos + buildingGap + 'vh';
+            buildingSprite.style.left = '100vw';
+            buildingSprite.increaseScore = '1';
+
+            document.body.appendChild(buildingSprite);
+        }
+        buildingSeparation++;
+        requestAnimationFrame(createBuilding);
+    }
+    requestAnimationFrame(createBuilding);
+}
---
+++ login.js
@@ -0,0 +1,11 @@
+function redirectToInstructions() {
+    const nameInput = document.querySelector('input[name="name"]');
+    const nicknameInput = document.querySelector('input[name="nickname"]');
+
+    if (nameInput.value && nicknameInput.value) {
+        // Both fields are filled, proceed to the other webpage
+        window.location.href = '../Instructions.html'; 
+    } else {
+        alert('✈️ Please enter your Name and Nickname to play ✈️');
+    }
+}
